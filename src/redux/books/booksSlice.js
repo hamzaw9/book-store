ce780dfb8c64c69f815cf12a8ee25bb5ee745a93
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-10 21:49:28 +0500

    Set up the initialState in booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,23 +1,36 @@
-// Change 1
+/* eslint-disable implicit-arrow-linebreak */
 import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState: { list: [] },
+  initialState,
   reducers: {
     addBook: (state, action) => {
-      state.list = [
-        ...state.list,
-        {
-          id: Math.floor(Math.random() * 100),
-          title: action.payload.title,
-          author: action.payload.author,
-        },
-      ];
+      state.push(action.payload);
     },
-    removeBook: (state, action) => {
-      state.list = state.list.filter((book) => book.id !== action.payload.id);
-    },
+    removeBook: (state, action) =>
+      state.filter((book) => book.item_id !== action.payload),
   },
 });
 
