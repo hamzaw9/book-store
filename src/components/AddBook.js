c73bef86620beb62ed1a35b350abf439f4e8b8dc
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-10 22:05:05 +0500

    Add event handlers to add and remove book from books array

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,12 +1,40 @@
-const AddBook = () => (
-  <div className="form-container">
-    <form>
-      <input className="input-text" type="text" required />
-      <input className="input-text" type="text" required />
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
-      <input className="input-submit" type="submit" value="ADD BOOK" />
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+
+    const title = e.target.elements.title.value;
+    const author = e.target.elements.author.value;
+
+    dispatch(addBook({ title, author }));
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleAddBook}>
+        <input
+          className="input-text"
+          type="text"
+          name="title"
+          placeholder="Title"
+          required
+        />
+        <input
+          className="input-text"
+          type="text"
+          name="author"
+          placeholder="Author"
+          required
+        />
+        <input className="input-submit" type="submit" value="ADD BOOK" />
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,29 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 import Book from './Book';
 
 const BookList = () => {
-  const books = [
-    { title: 'Mindset', author: 'Shadman', id: 0 },
-    { title: 'Eat that frog', author: 'Naresh', id: 1 },
-  ];
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = (id) => {
+    dispatch(removeBook(id));
+  };
 
   return (
     <div>
       {books.map((book) => (
-        <Book key={book.id} title={book.title} author={book.author} />
+        <div key={book.item_id}>
+          <Book
+            title={book.title}
+            author={book.author}
+            category={book.category}
+          />
+          <button type="button" onClick={() => handleRemoveBook(book.item_id)}>
+            Remove Book
+          </button>
+        </div>
       ))}
     </div>
   );
